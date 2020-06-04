fa9a929fcd9e624f62a611a23e120c44b9c19f5f
pooja233 <66385585+pooja233@users.noreply.github.com>
2020-06-04 09:30:23 +0530

    Add files via upload

---
+++ src/serve-dev.vue
@@ -0,0 +1,70 @@
+<script>
+import Vue from 'vue'
+import BootstrapSidebar from '@/vue-bootstrap-sidebar.vue'
+
+export default Vue.extend({
+  name: 'App',
+  components: {
+    BootstrapSidebar
+  },
+  data () {
+    return {
+      initialShow: true,
+      header: '<h3>Sidebar</h3>',
+      links: [
+        { name: 'Home', href: { name: 'home' }, faIcon: ['fas', 'home'] },
+
+        
+           
+        { name: 'About', href: { name: 'about' }, faIcon: 'users' },
+        { name: 'Contact', href: { name: 'contact' }, faIcon: 'phone' },
+        { name: 'Export', href: { name: 'Export' } },
+      ]
+    }
+  },
+  methods: {
+    onSidebarChanged () {
+    }
+  }
+});
+</script>
+
+<template>
+  <div id="App">
+    <BootstrapSidebar 
+      :initial-show="initialShow" 
+      :links="links"
+      :header="header"
+      :fa="true"
+      @sidebarChanged="onSidebarChanged"
+    >
+      <template v-slot:navbar>
+        <b-navbar 
+          id="mainNavbar" 
+          toggleable="lg" 
+          type="light" 
+          variant="light" 
+          fixed="top"
+        >
+          <b-navbar-nav>
+            <b-nav-item>
+              Navbar
+            </b-nav-item>
+          </b-navbar-nav>
+        </b-navbar>
+      </template>
+      
+      <template v-slot:content>
+        <b-container style="margin-top: 56px">
+          <router-view />
+        </b-container>
+      </template>
+    </BootstrapSidebar>
+  </div>
+</template>
+
+<style lang="scss">
+@import 'node_modules/bootstrap/scss/bootstrap';
+@import 'node_modules/bootstrap-vue/src/index.scss';
+@import './scss/default-theme';
+</style>
